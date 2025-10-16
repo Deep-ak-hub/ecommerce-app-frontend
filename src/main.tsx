e3ba3e44f2ebaeb852b28e2b454a1dd068d6c4d2
Deepak Joshi <joshideepu892@gmail.com>
2025-10-16 09:13:48 +0545

    initial frontend setup

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import { createRoot } from "react-dom/client";
+
+/* const rootDOM = document.getElementById("root")
+const reactDOM = createRoot(rootDOM!)
+
+reactDOM.render(
+  <h1>Hello World</h1>
+) */
+
+/* createRoot(document.getElementById("root")!).render(
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+      ducimus praesentium est iure nulla aspernatur, tenetur nostrum sapiente
+      officia reprehenderit totam iusto amet error ea facere doloremque? Fugit,
+      voluptatum laudantium.
+    </p>
+  </div>
+);
+*/
+
+// React Component
+function App() {
+  return (
+    <div>
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus
+      ducimus praesentium est iure nulla aspernatur, tenetur nostrum sapiente
+      officia reprehenderit totam iusto amet error ea facere doloremque? Fugit,
+      voluptatum laudantium.
+    </p>
+  </div>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(
+  <App />
+)
